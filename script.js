7e67932390df8391f7f6436fb4bf00efd9c38dc3
Kirill Ignatov <ika22.jordan@gmail.com>
2024-05-16 01:50:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function showSections(firstSectionId, secondSectionId) {
-    var sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        section.style.display = 'none'; // Скрыть все секции
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const nextSibling = target.nextElementSibling || target.parentElement.firstElementChild; // Если следующий элемент отсутствует, выбираем первый элемент
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+        setTimeout(() => { // Небольшая задержка для плавной прокрутки
+            window.scrollBy({
+                top: nextSibling.getBoundingClientRect().top - window.innerHeight / 2,
+                behavior: 'smooth'
+            });
+        }, 600);
     });
-    document.getElementById(firstSectionId).style.display = 'block'; // Показать первую секцию
-    document.getElementById(secondSectionId).style.display = 'block'; // Показать вторую секцию
-}
-
+});
